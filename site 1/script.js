46837add0d33968224bf5f3aa34dec83b4d22bc3
phfarath <pedropontesfarath@gmail.com>
2023-05-12 10:35:54 -0300

    js-1205-data

---
+++ site 1/script.js
@@ -0,0 +1,12 @@
+//retornando a data
+let horas =new Date();
+document.write(horas.getHours());
+document.write("<br>");
+///RETORNANDO minutos 
+let min=new Date();
+document.write(min.getMinutes());
+document.write("<br>");
+//retornando segundos 
+let seg=new Date();
+document.write(seg.getSeconds());
+document.write("<br>");
